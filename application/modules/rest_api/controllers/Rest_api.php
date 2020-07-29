dadfddcb20e105b249247dc9b37e1ec6e5a0c177
codingindoId <wearedevev@gmail.com>
2020-07-29 11:53:42 +0700

    fix api input rutin

--- application/modules/rest_api/controllers/Rest_api.php
+++ application/modules/rest_api/controllers/Rest_api.php
@@ -43,39 +43,31 @@ class Rest_api extends MY_Controller {
 			'no_pol' => $this->input->post('no_pol')
 		);
 
-		$cek_masuk = $this->M_api->cek_no_pol($where)->result();
-		if($cek_masuk){
+		/*mengambil detil kendaraan*/
+		$detil = $this->M_api->get_kendaraan_by_id($where)->row();
+		$data = array(
+			'id_kunjungan'		=> "KUNJ-".uniqid(6),
+			'no_pol'			=> $detil->no_pol,
+			'tgl_kunjungan'		=> date('Y-m-d'),
+			'jam_masuk'			=> date('H:i:s'),
+			'jenis_kunjungan'	=> 'rutin',
+			'driver'			=> $detil->pemilik,
+			'tahun'				=> date('Y'),
+			'bulan'				=> date('m')
+		);
+		$cek2 = $this->M_api->tambah_kunjungan($data);
+		if (!$cek2) {
 			echo json_encode(array(
-				'status' 	=> 0,
-				'message'	=> 'ada kunjungan yang belum terselesaikan, anda tidak bisa memasukkan nomor polisi ini sebelum kendaraan tersebut menyelesaikan kunjungannya'
+				'success'	=> 1,
+				'message'	=> 'kendaraan dengan nomor polisi '. $this->input->post('no_pol').', diijinkan Masuk.'
 			));
 		}else{
-			/*mengambil detil kendaraan*/
-			$detil = $this->M_api->get_kendaraan_by_id($where)->row();
-			
-			$data = array(
-				'id_kunjungan'		=> "KUNJ-".uniqid(6),
-				'no_pol'			=> $detil->no_pol,
-				'tgl_kunjungan'		=> date('Y-m-d'),
-				'jam_masuk'			=> date('H:i:s'),
-				'jenis_kunjungan'	=> 'rutin',
-				'driver'			=> $detil->pemilik,
-				'tahun'				=> date('Y'),
-				'bulan'				=> date('m')
-			);
-			$cek2 = $this->M_api->tambah_kunjungan($data);
-			if (!$cek2) {
-				echo json_encode(array(
-					'success'	=> 1,
-					'message'	=> 'kendaraan dengan nomor polisi '. $this->input->post('no_pol').', diijinkan Masuk.'
-				));
-			}else{
-				echo json_encode(array(
-					'success'	=> 0,
-					'message'	=> 'ups, ada yang salah'
-				));
-			}
+			echo json_encode(array(
+				'success'	=> 0,
+				'message'	=> 'ups, ada yang salah'
+			));
 		}
+		
 	}
 
 
